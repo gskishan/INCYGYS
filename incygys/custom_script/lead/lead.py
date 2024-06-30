6844736ca618b86c3dccbdb4e1470d8255fa36ea
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-06-30 22:25:03 +0530

    Update lead.py

--- incygys/custom_script/lead/lead.py
+++ incygys/custom_script/lead/lead.py
@@ -2,17 +2,22 @@ import frappe
 
 @frappe.whitelist()
 def create_opportunity_from_lead(lead_name):
-    lead = frappe.get_doc("Lead", lead_name)
-    if lead.custom_lead_status == "Interested":
-        opportunity = frappe.new_doc("Opportunity")
-        opportunity.party_name = lead.name
-        opportunity.custom_source_type = lead.custom_source_type
-        opportunity.contact_email = lead.custom_email
-        opportunity.contact_mobile = lead.mobile_no
-        opportunity.opportunity_from = "Lead"
-        opportunity.insert()
-        frappe.msgprint(f"Opportunity {opportunity.name} created from Lead {lead.name}")
-        return opportunity
+    try:
+        lead = frappe.get_doc("Lead", lead_name)
+        if lead.custom_lead_status == "Interested":
+            opportunity = frappe.new_doc("Opportunity")
+            opportunity.party_name = lead.name
+            opportunity.custom_source_type = lead.custom_source_type
+            opportunity.contact_email = lead.email_id
+            opportunity.contact_mobile = lead.mobile_no
+            opportunity.opportunity_from = "Lead"
+            opportunity.insert()
+            frappe.msgprint(f"Opportunity {opportunity.name} created from Lead {lead.name}")
+            return opportunity
+    except frappe.DoesNotExistError:
+        frappe.msgprint(f"Lead {lead_name} not found")
+    except Exception as e:
+        frappe.log_error(f"An error occurred while creating an opportunity from lead {lead_name}: {str(e)}")
 
 def lead_validate(doc, method):
     if doc.custom_lead_status == "Interested":
