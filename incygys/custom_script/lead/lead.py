10070ea24ac55766f3fe3007a274ccd1d03fb3b5
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-07-02 18:51:20 +0530

    Update lead.py

--- incygys/custom_script/lead/lead.py
+++ incygys/custom_script/lead/lead.py
@@ -7,12 +7,12 @@ def create_opportunity_on_lead_status(doc, method):
     if doc.custom_lead_status == "Interested":
         # Create a new Opportunity
         opportunity = frappe.new_doc("Opportunity")
-        opportunity.party_name = lead.name
-        opportunity.custom_source_type = lead.custom_source_type
-        opportunity.contact_email = lead.email_id
-        opportunity.contact_mobile = lead.mobile_no
+        opportunity.party_name = doc.name
+        opportunity.custom_source_type = doc.custom_source_type
+        opportunity.contact_email = doc.email_id
+        opportunity.contact_mobile = doc.mobile_no
         opportunity.opportunity_from = "Lead"
-        opportunity.company = lead.custom_sales_organization
+        opportunity.company = doc.custom_sales_organization
 
         # Save the Opportunity
         opportunity.insert()
@@ -21,4 +21,6 @@ def create_opportunity_on_lead_status(doc, method):
         # Optionally, you can link this opportunity back to the lead
         doc.opportunity = opportunity.name
         doc.save()
+        
+        # Notify the user that the Opportunity has been created
         frappe.msgprint(f'Opportunity {opportunity.name} has been created for Lead {doc.name}')
