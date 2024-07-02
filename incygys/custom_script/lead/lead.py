4c8c7bce6f1366e186f549a1db6efa86d31f6008
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-07-02 18:59:36 +0530

    Update lead.py

--- incygys/custom_script/lead/lead.py
+++ incygys/custom_script/lead/lead.py
@@ -3,9 +3,7 @@ from erpnext.crm.doctype.opportunity.opportunity import Opportunity
 
 @frappe.whitelist()
 def create_opportunity_on_lead_status(doc, method):
-    # Check if the lead status is 'Interested'
     if doc.custom_lead_status == "Interested":
-        # Create a new Opportunity
         opportunity = frappe.new_doc("Opportunity")
         opportunity.party_name = doc.name
         opportunity.custom_source_type = doc.custom_source_type
@@ -14,13 +12,11 @@ def create_opportunity_on_lead_status(doc, method):
         opportunity.opportunity_from = "Lead"
         opportunity.company = doc.custom_sales_organization
 
-        # Save the Opportunity
         opportunity.insert()
         frappe.db.commit()
 
-        # Optionally, you can link this opportunity back to the lead
+        
         doc.opportunity = opportunity.name
         doc.save()
 
-        # Notify the user that the Opportunity has been created
         frappe.msgprint(f'Opportunity {opportunity.name} has been created for Lead {doc.name}')
