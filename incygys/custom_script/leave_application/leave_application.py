773762ee6cb10b4d175aa32f6c10e5e3455f6ec5
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-07-03 15:39:07 +0530

    Update leave_application.py

--- incygys/custom_script/leave_application/leave_application.py
+++ incygys/custom_script/leave_application/leave_application.py
@@ -6,12 +6,22 @@ from frappe.utils import (
 	getdate
 )
 class CustomLeaveApplication(LeaveApplication):
-  def create_or_update_attendance(self, attendance_name, date):
+	def create_or_update_attendance(self, attendance_name, date):
 		status = (
 			"Half Day" if self.half_day_date and getdate(date) == getdate(self.half_day_date) else "On Leave"
 		)
+		sql=""" select *  from  `tabLeave Application` where employee="{0}" and from_date="{1}" and to_date ="{2}" and status="Approved" """.format(self.employee,self.from_date,self.to_date)
+		half_day_date_count=0
+		for d in (frappe.db.sql(sql,as_dict=True)):
+			if d.half_day:
+				half_day_date_count+=1
+
 
 		if attendance_name:
+			if status=="Half Day":
+				if half_day_date_count==2:
+					status="On Leave"
+
 			# update existing attendance, change absent to on leave
 			doc = frappe.get_doc("Attendance", attendance_name)
 			doc.db_set({"status": status, "leave_type": self.leave_type, "leave_application": self.name})
