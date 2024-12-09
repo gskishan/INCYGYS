350df2e31d83aa79eb023678b82f2f788fed112c
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-12-09 15:50:48 +0530

    Update lead_report.py

--- incygys/incygys/report/lead_report/lead_report.py
+++ incygys/incygys/report/lead_report/lead_report.py
@@ -2,44 +2,44 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
-    query = """
-    SELECT
-        `tabLead`.`custom_sales_organization` AS "Sales Organization",
-        `tabLead`.`lead_name` AS "Lead Name",
-        `tabLead`.`custom_mobile_numbers` AS "Mobile Numbers",
-        `tabLead`.`custom_email` AS "Email",
-        `tabLead`.`country` AS "Country",
-        `tabLead`.`custom_state_copy` AS "State",
-        `tabLead`.`custom_city_name` AS "City",
-        `tabLead`.`custom_address` AS "Address",
-        `tabLead`.`custom_lead_status` AS "Lead Status",
-        `tabLead`.`custom_source_type` AS "Source Type",
-        `tabLead`.`custom_architect_is` AS "Architect",
-        `tabLead`.`owner` AS "Lead Owner",
-        `tabLead`.`custom_owner_name` AS "Owner Name",
-        `tabLead`.`custom_property_type` AS "Property Type",
-        `tabLead`.`custom_sq_ft` AS "Size sq ft",
-        `tabLead`.`custom_requirement_stage_date` AS "Requirement Stage Date",
-        `tabLead`.`custom_supervisor_name` AS "Supervisor Name",
-        `tabLead`.`custom_supervisor_phone` AS "Supervisor Phone",
-        `tabLead`.`email_id` AS "Email ID",
-        CASE 
-            WHEN `tabLead`.`custom_source_type` = 'Architect' THEN `tabLead`.`custom_name`
-            WHEN `tabLead`.`custom_source_type` = 'Contractor' THEN `tabLead`.`custom_contractor_name`
-            WHEN `tabLead`.`custom_source_type` = 'Dealer' THEN `tabLead`.`custom_dealer_name`
-            WHEN `tabLead`.`custom_source_type` = 'Others' THEN `tabLead`.`custom_reference_name`
-            WHEN `tabLead`.`custom_source_type` = 'Consultant' THEN `tabLead`.`custom_consultant_names`
-            WHEN `tabLead`.`custom_source_type` = 'Event/Exhibition' THEN `tabLead`.`custom_eventexhibition_type`
-            WHEN `tabLead`.`custom_source_type` = 'Builder' THEN `tabLead`.`custom_builder_name`
-            ELSE NULL
-        END AS "Lead Source Name"
-    FROM
-        `tabLead`
-    WHERE
-        `tabLead`.`custom_lead_status` = %(status)s
-        AND `tabLead`.`creation` BETWEEN %(from_date)s AND %(to_date)s
-    ORDER BY
-        `tabLead`.`creation` DESC
-    """
-    data = frappe.db.sql(query, filters, as_dict=True)
-    return [], data
+    # query = """
+    # SELECT
+    #     `tabLead`.`custom_sales_organization` AS "Sales Organization",
+    #     `tabLead`.`lead_name` AS "Lead Name",
+    #     `tabLead`.`custom_mobile_numbers` AS "Mobile Numbers",
+    #     `tabLead`.`custom_email` AS "Email",
+    #     `tabLead`.`country` AS "Country",
+    #     `tabLead`.`custom_state_copy` AS "State",
+    #     `tabLead`.`custom_city_name` AS "City",
+    #     `tabLead`.`custom_address` AS "Address",
+    #     `tabLead`.`custom_lead_status` AS "Lead Status",
+    #     `tabLead`.`custom_source_type` AS "Source Type",
+    #     `tabLead`.`custom_architect_is` AS "Architect",
+    #     `tabLead`.`owner` AS "Lead Owner",
+    #     `tabLead`.`custom_owner_name` AS "Owner Name",
+    #     `tabLead`.`custom_property_type` AS "Property Type",
+    #     `tabLead`.`custom_sq_ft` AS "Size sq ft",
+    #     `tabLead`.`custom_requirement_stage_date` AS "Requirement Stage Date",
+    #     `tabLead`.`custom_supervisor_name` AS "Supervisor Name",
+    #     `tabLead`.`custom_supervisor_phone` AS "Supervisor Phone",
+    #     `tabLead`.`email_id` AS "Email ID",
+    #     CASE 
+    #         WHEN `tabLead`.`custom_source_type` = 'Architect' THEN `tabLead`.`custom_name`
+    #         WHEN `tabLead`.`custom_source_type` = 'Contractor' THEN `tabLead`.`custom_contractor_name`
+    #         WHEN `tabLead`.`custom_source_type` = 'Dealer' THEN `tabLead`.`custom_dealer_name`
+    #         WHEN `tabLead`.`custom_source_type` = 'Others' THEN `tabLead`.`custom_reference_name`
+    #         WHEN `tabLead`.`custom_source_type` = 'Consultant' THEN `tabLead`.`custom_consultant_names`
+    #         WHEN `tabLead`.`custom_source_type` = 'Event/Exhibition' THEN `tabLead`.`custom_eventexhibition_type`
+    #         WHEN `tabLead`.`custom_source_type` = 'Builder' THEN `tabLead`.`custom_builder_name`
+    #         ELSE NULL
+    #     END AS "Lead Source Name"
+    # FROM
+    #     `tabLead`
+    # WHERE
+    #     `tabLead`.`custom_lead_status` = %(status)s
+    #     AND `tabLead`.`creation` BETWEEN %(from_date)s AND %(to_date)s
+    # ORDER BY
+    #     `tabLead`.`creation` DESC
+    # """
+    # data = frappe.db.sql(query, filters, as_dict=True)
+    return [], []
