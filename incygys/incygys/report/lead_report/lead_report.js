a749b64528d3cd6365246093e2e1cdb6bee9c16d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-12-09 15:48:29 +0530

    Update lead_report.js

--- incygys/incygys/report/lead_report/lead_report.js
+++ incygys/incygys/report/lead_report/lead_report.js
@@ -1,30 +1,30 @@
 frappe.query_reports["Lead Report"] = {
-    "filters": [
-        {
-            "fieldname": "from_date",
-            "label": __("From Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.month_start(),  // Optional: Current month's start date
-            "reqd": 1 // Required field
-        },
-        {
-            "fieldname": "to_date",
-            "label": __("To Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.month_end(),  // Optional: Current month's end date
-            "reqd": 1 // Required field
-        },
-        {
-            "fieldname": "custom_lead_status",
-            "label": __("Custom Lead Status"),
-            "fieldtype": "Select",
-            "options": [
-                "Potential",
-                "Interested",
-                "Unresponded",
-                "Not-Interested"
-            ], // Defined options for your custom lead status
-            "default": "Potential" // Optional default value
-        }
-    ]
+    // "filters": [
+    //     {
+    //         "fieldname": "from_date",
+    //         "label": __("From Date"),
+    //         "fieldtype": "Date",
+    //         "default": frappe.datetime.month_start(),  // Optional: Current month's start date
+    //         "reqd": 1 // Required field
+    //     },
+    //     {
+    //         "fieldname": "to_date",
+    //         "label": __("To Date"),
+    //         "fieldtype": "Date",
+    //         "default": frappe.datetime.month_end(),  // Optional: Current month's end date
+    //         "reqd": 1 // Required field
+    //     },
+    //     {
+    //         "fieldname": "custom_lead_status",
+    //         "label": __("Custom Lead Status"),
+    //         "fieldtype": "Select",
+    //         "options": [
+    //             "Potential",
+    //             "Interested",
+    //             "Unresponded",
+    //             "Not-Interested"
+    //         ], // Defined options for your custom lead status
+    //         "default": "Potential" // Optional default value
+    //     }
+    // ]
 };
