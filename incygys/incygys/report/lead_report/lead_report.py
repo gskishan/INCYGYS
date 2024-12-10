bd3d28d7f50bd4955257b81d92f411af54857d90
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-12-10 18:46:28 +0530

    Update lead_report.py

--- incygys/incygys/report/lead_report/lead_report.py
+++ incygys/incygys/report/lead_report/lead_report.py
@@ -2,6 +2,10 @@ import frappe
 from frappe import _
 
 def execute(filters=None):
+    # Ensure filters are passed to avoid issues
+    if not filters:
+        filters = {}
+
     query = """
     SELECT
         `tabLead`.`custom_sales_organization` AS "Sales Organization",
@@ -23,6 +27,8 @@ def execute(filters=None):
         `tabLead`.`custom_supervisor_name` AS "Supervisor Name",
         `tabLead`.`custom_supervisor_phone` AS "Supervisor Phone",
         `tabLead`.`email_id` AS "Email ID",
+        `tabLead`.`creation` AS "Creation Date",
+        `tabLead`.`modified` AS "Last Modified",
         CASE 
             WHEN `tabLead`.`custom_source_type` = 'Architect' THEN `tabLead`.`custom_name`
             WHEN `tabLead`.`custom_source_type` = 'Contractor' THEN `tabLead`.`custom_contractor_name`
@@ -41,5 +47,31 @@ def execute(filters=None):
     ORDER BY
         `tabLead`.`creation` DESC
     """
+    # Execute query with filters
     data = frappe.db.sql(query, filters, as_dict=True)
-    return data
+    # Define columns for report
+    columns = [
+        {"fieldname": "Sales Organization", "label": _("Sales Organization"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Lead Name", "label": _("Lead Name"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Mobile Numbers", "label": _("Mobile Numbers"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Email", "label": _("Email"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Country", "label": _("Country"), "fieldtype": "Data", "width": 120},
+        {"fieldname": "State", "label": _("State"), "fieldtype": "Data", "width": 120},
+        {"fieldname": "City", "label": _("City"), "fieldtype": "Data", "width": 120},
+        {"fieldname": "Address", "label": _("Address"), "fieldtype": "Data", "width": 200},
+        {"fieldname": "Lead Status", "label": _("Lead Status"), "fieldtype": "Select", "width": 120},
+        {"fieldname": "Source Type", "label": _("Source Type"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Architect", "label": _("Architect"), "fieldtype": "Data", "width": 100},
+        {"fieldname": "Lead Owner", "label": _("Lead Owner"), "fieldtype": "Link", "options": "User", "width": 120},
+        {"fieldname": "Owner Name", "label": _("Owner Name"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Property Type", "label": _("Property Type"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Size sq ft", "label": _("Size sq ft"), "fieldtype": "Float", "width": 120},
+        {"fieldname": "Requirement Stage Date", "label": _("Requirement Stage Date"), "fieldtype": "Date", "width": 120},
+        {"fieldname": "Supervisor Name", "label": _("Supervisor Name"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Supervisor Phone", "label": _("Supervisor Phone"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Email ID", "label": _("Email ID"), "fieldtype": "Data", "width": 150},
+        {"fieldname": "Creation Date", "label": _("Creation Date"), "fieldtype": "Date", "width": 120},
+        {"fieldname": "Last Modified", "label": _("Last Modified"), "fieldtype": "Date", "width": 120},
+        {"fieldname": "Lead Source Name", "label": _("Lead Source Name"), "fieldtype": "Data", "width": 150}
+    ]
+    return columns, data
