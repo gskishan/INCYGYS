d2887c38959d499010327c0d81b0eb7712430d2d
G S Kishan <95840962+gskishan@users.noreply.github.com>
2024-12-10 18:57:57 +0530

    Update opportunity_report.js

--- incygys/incygys/report/opportunity_report/opportunity_report.js
+++ incygys/incygys/report/opportunity_report/opportunity_report.js
@@ -1,1 +1,29 @@
-
+frappe.query_reports["Lead Report"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_start(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.month_end(),
+            "reqd": 1
+        },
+        {
+            "fieldname": "status",
+            "label": __("Opportunity Status"),
+            "fieldtype": "Select",
+            "options": [
+                "Quotation",
+                "Converted",
+                "Lost"
+            ],
+            "default": "Quotation"
+        }
+    ]
+};
